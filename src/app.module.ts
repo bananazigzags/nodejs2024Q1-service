fc2a60d229ed56306da0395a400b07f15f91445d
Maria T <bananazigzags@gmail.com>
2024-03-09 16:35:46 +0300

    feat: favorites

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,17 @@ import { UserModule } from './user/user.module';
 import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { DbModule } from './db/db.module';
 @Module({
-  imports: [UserModule, TrackModule, ArtistModule, AlbumModule],
+  imports: [
+    UserModule,
+    TrackModule,
+    ArtistModule,
+    AlbumModule,
+    FavoritesModule,
+    DbModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
