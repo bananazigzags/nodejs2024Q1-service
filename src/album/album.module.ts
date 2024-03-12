e55c7bf40ef7c61eb3ee3d7ee67fcb1864db1dc2
Maria T <bananazigzags@gmail.com>
2024-03-12 23:52:37 +0300

    feat: migrate album resource to postgres db

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -3,11 +3,12 @@ import { AlbumController } from './album.controller';
 import { AlbumService } from './album.service';
 import { TrackModule } from 'src/track/track.module';
 import { DbModule } from 'src/db/db.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [AlbumController],
   providers: [AlbumService],
-  imports: [TrackModule, DbModule],
+  imports: [TrackModule, DbModule, PrismaModule],
   exports: [AlbumService],
 })
 export class AlbumModule {}
